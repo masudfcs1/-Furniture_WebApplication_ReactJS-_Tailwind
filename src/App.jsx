1cc6920ce317b2fcfde793b4f56000c282255022
masud rana <masudfcs1@gmail.com>
2023-08-17 11:10:22 +0600

    Updated

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <div className=" w-full max-w-[1440px] mx-auto bg-white ">
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
