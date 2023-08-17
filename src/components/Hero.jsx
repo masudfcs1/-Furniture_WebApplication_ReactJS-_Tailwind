c2548f33d7e84dadacc5f6f9cafbc4b604e92685
masud rana <masudfcs1@gmail.com>
2023-08-17 15:02:41 +0600

    Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { hero } from "../data";
 
 export default function Hero() {
-  return <div> Hero </div>;
+  const { title, subtitle, buttonText } = hero;
+
+  return (
+    <section className=" h-[850px] w-full bg-hero bg-right bg-cover bg-no-repeat text-white pt-[225px] pb-[254px] relative mb-12 lg:bg-center lg:mb-28 ">
+      <div className=" container mx-auto text-center">
+        <h1> {title} </h1>
+      </div>
+    </section>
+  );
 }
