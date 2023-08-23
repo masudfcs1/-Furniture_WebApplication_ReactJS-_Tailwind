285d1af65902f4d3dab0878bbc7dd47e8c29a009
masud rana <masudfcs1@gmail.com>
2023-08-23 11:58:26 +0600

    Features

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -10,17 +10,19 @@ const Features = () => {
           <div className=" flex-1 order-1 lg:-order-1">
             <img src={image.type} alt="" />
           </div>
-          <div>
-            <h2> {title} </h2>
-            <p> {subtitle} </p>
+          <div className=" flex-1 flex flex-col justify-end">
+            <h2 className=" title "> {title} </h2>
+            <p className="subtitle"> {subtitle} </p>
             <div>
               {items.map((item, index) => {
                 const { icon, title, subtitle } = item;
                 return (
-                  <div key={index}>
-                    <div> {icon} </div>
-                    <div>
-                      <h4>{title}</h4>
+                  <div key={index} className=" flex mb-6 lg:mb-0">
+                    <div className=" text-2xl mr-4 lg:text-3xl"> {icon} </div>
+                    <div className=" mb-4">
+                      <h4 className=" text-base lg:text-xl font-semibold ">
+                        {title}
+                      </h4>
                       <p>{subtitle} </p>
                     </div>
                   </div>
