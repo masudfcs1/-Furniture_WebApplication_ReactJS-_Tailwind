db3747133f6f62b2b0248220f0e35729a9a5ea28
masud rana <masudfcs1@gmail.com>
2023-08-23 17:53:38 +0600

    NewItemsSlider

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
+import NewItems from "./components/NewItems";
 
 function App() {
   return (
     <div className=" w-full max-w-[1440px] mx-auto bg-white ">
       <Hero />
       <Features />
+      <NewItems />
     </div>
   );
 }
--- src/components/NewItemsSlider.jsx
+++ src/components/NewItemsSlider.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+
+// Import Swiper styles
+
+import { newInStore } from "../data";
 
 const NewItemsSlider = () => {
-  return <div> NewItemsSlider </div>;
+  return (
+    <Swiper>
+      {newInStore.products.map((product, index) => {
+        return (
+          <SwiperSlide className=" max-w-[275px]" key={index}>
+            {" "}
+            <div>
+              <img src={product.image.type} alt="" />
+              <div>{product.name}</div>
+            </div>{" "}
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default NewItemsSlider;
